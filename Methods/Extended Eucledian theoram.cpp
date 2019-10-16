c192877cc0ec3d18218141626b80e1e4e4a23233
Manas Bhardwaj <manas1109bhardwaj@gmail.com>
2019-10-16 16:16:57 +0530

    Added Eucledian Extended theorem.

---
+++ Methods/Extended Eucledian theoram.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int GCD(int a, int b, int &x, int &y){
+    if(a==0){
+        x = 0;
+        y = 1;
+        return b;
+    }
+    int x1,y1;
+    int ans = GCD(b%a,a,x1,y1);
+    x = y1 - (b/a) * x1;
+    y = x1;
+    return ans;
+}
+
+int main(){
+    int a=4,b=6,x,y;
+    cout<<GCD(a,b,x,y)<<endl;
+    cout<<x<<" "<<y<<endl;
+}
