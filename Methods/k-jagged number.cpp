8a3c0ab3ebcf5b534c825a8290be3f0949efbdfd
Manas Bhardwaj <manas1109bhardwaj@gmail.com>
2019-11-04 10:55:17 +0530

    Updated code k-jagged number

--- Methods/k-jagged number.cpp
+++ Methods/k-jagged number.cpp
@@ -1,25 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
-//Wrong code check geeks for geeks.
 // k-jagged number is a number if smallest prime factor >= k.
 // first ten 7-jagged number.
-bool isPrime(long long int n){
-    if (n<2) return false;
-    for(int i=2;i*i<=n;i++)
-        if(n%i==0) return false;
-    return true;
-}
+// A k-rough or k-jagged number is a number whose smallest prime factor
+// is greater than or equal to the number ‘k’.
+// Returns true if n is k rough else false
+bool isJagged(int n, int k)
+{
+    // If n is even, then smallest prime
+    // factor becomes 2.
+    if (n % 2 == 0)
+        return (k <= 2);
 
-bool isJagged(int x,int k){
-	int temp=3;
-	while(temp!=x){
-		if(x%temp==0 && isPrime(temp)){
-			if(temp >= k) return true;
-		}
-		temp++;
-	}
-//	if(temp <= p) return true;
-	return false;
+    // n must be odd at this point.  So we
+    // can skip one element (Note i = i +2)
+    for (int i = 3; i*i <= n; i = i+2)
+        if (n%i == 0)
+            return (i >= k);
+
+   return (n >= k);
 }
 
 
